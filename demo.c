ef7a425c323aa7ed3ce152d62a8c2f45675a634f
Hiep Hoang <cnhh85@gmail.com>
2022-05-26 12:00:40 +0700

    f3 done

--- demo.c
+++ demo.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
+void f3()
+{
+    int a, b;
+    printf("Dien 2 so: ");
+    scanf("%d%d", &a, &b);
+    printf("a + b = %d \n", a - b);
+}
 int main()
 {
     printf("Hello F-Code:\n");
-    f1();
-    f2();
+    // f1();
+    // f2();
     f3();
-    f4();
+    // f4();
     return 0;
 }
