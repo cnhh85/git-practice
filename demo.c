075b055465e3cccd052d1b6b1d7e873a4c8de182
Hiep Hoang <cnhh85@gmail.com>
2022-05-26 11:54:17 +0700

    f1 done

--- demo.c
+++ demo.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-
+void f1()
+{
+    printf("Nhóm...\n");
+}
 int main()
+
 {
     printf("Hello F-Code:\n");
     f1();
-    f2();
-    f3();
-    f4();
+    // f2();
+    // f3();
+    // f4();
     return 0;
 }
